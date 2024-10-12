51af272af4d80e1f94527015cfa921a978aa469f
Szymon Kowaliński <szymon@kowalinski.dev>
2024-10-12 13:06:30 +0200

    feat: small theme chanfges

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,81 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ShadApp(
       theme: ShadThemeData(
-        brightness: Brightness.light,
-        colorScheme: const ShadColorScheme(
-          background: CVColors.floralWhite,
-          foreground: CVColors.richBlack,
-          card: CVColors.siennaBrown,
-          cardForeground: CVColors.richBlack,
-          popover: CVColors.siennaBrown,
-          popoverForeground: CVColors.richBlack,
-          primary: CVColors.orangeWheel,
-          primaryForeground: CVColors.richBlack,
-          secondary: CVColors.caribbeanGreen,
-          secondaryForeground: CVColors.richBlack,
-          muted: CVColors.platinumLightGrey,
-          mutedForeground: CVColors.richBlack,
-          accent: CVColors.caribbeanGreen,
-          accentForeground: CVColors.richBlack,
-          destructive: CVColors.burgundyRed,
-          destructiveForeground: CVColors.floralWhite,
-          border: CVColors.siennaBrown,
-          input: CVColors.richBlack,
-          ring: CVColors.caribbeanGreen,
-          selection: CVColors.caribbeanGreen,
+          brightness: Brightness.light,
+          colorScheme: const ShadColorScheme(
+            background: CVColors.floralWhite,
+            foreground: CVColors.richBlack,
+            card: CVColors.siennaBrown,
+            cardForeground: CVColors.richBlack,
+            popover: CVColors.siennaBrown,
+            popoverForeground: CVColors.richBlack,
+            primary: CVColors.orangeWheel,
+            primaryForeground: CVColors.floralWhite,
+            secondary: CVColors.siennaBrown,
+            secondaryForeground: CVColors.richBlack,
+            muted: CVColors.platinumLightGrey,
+            mutedForeground: CVColors.richBlack,
+            accent: CVColors.siennaBrown,
+            accentForeground: CVColors.floralWhite,
+            destructive: CVColors.burgundyRed,
+            destructiveForeground: CVColors.floralWhite,
+            border: CVColors.siennaBrown,
+            input: CVColors.richBlack,
+            ring: Colors.transparent,
+            selection: CVColors.platinumLightGrey,
+          ),
+          inputTheme: ShadInputTheme(
+            decoration: ShadDecoration(
+              border: ShadBorder.all(color: CVColors.platinumLightGrey),
+              focusedBorder: ShadBorder.all(color: CVColors.orangeWheel),
+              errorBorder: ShadBorder.all(),
+            ),
+          )),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ShadButton(
+                child: const Text('Primary'),
+                onPressed: () {},
+              ),
+              ShadButton.outline(
+                child: const Text('Add to calendar'),
+                onPressed: () {
+                  ShadToaster.of(context).show(
+                    ShadToast(
+                      title: const Text('Scheduled: Catch up'),
+                      description:
+                          const Text('Friday, February 10, 2023 at 5:57 PM'),
+                      action: ShadButton.outline(
+                        child: const Text('Undo'),
+                        onPressed: () => ShadToaster.of(context).hide(),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              ShadInput(
+                placeholder: const Text('Password'),
+                obscureText: false,
+                prefix: const Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: ShadImage.square(size: 16, LucideIcons.lock),
+                ),
+                suffix: ShadButton(
+                  width: 24,
+                  height: 24,
+                  padding: EdgeInsets.zero,
+                  icon: const ShadImage.square(
+                    size: 16,
+                    false ? LucideIcons.eyeOff : LucideIcons.eye,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
