ce5859db40789636193cfc875f88cf0dc0f7ce80
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-10-12 18:07:19 +0200

    style ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:cvelocity/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'features/bottom-nav-bar/bottom_navbar_controller.dart';
 import 'features/bottom-nav-bar/main_widget.dart';
 import 'theme/app_theme.dart';
+import 'dart:ui' as ui;
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -16,12 +18,18 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      theme: ThemeData(extensions: const [AppTheme()]),
-      home: const Scaffold(
-        body: MainWidget(),
-        bottomNavigationBar: MyBottomNavBar(),
-      ),
-    );
+        theme: ThemeData(extensions: const [AppTheme()]),
+        home: const Stack(
+          children: [
+            MainWidget(),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: MyBottomNavBar(),
+            ),
+          ],
+        ));
   }
 }
 
@@ -33,21 +41,48 @@ class MyBottomNavBar extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final activeTab = ref.watch(bottomNavbarControllerProvider);
-    return BottomNavigationBar(
-      currentIndex: activeTab.index,
-      onTap: (value) => ref
-          .read(bottomNavbarControllerProvider.notifier)
-          .setTab(BottomNavbarEnum.values[value]),
-      items: [
-        ...BottomNavbarEnum.values.map(
-          (e) => BottomNavigationBarItem(
-            icon: Icon(e.icon),
-            label: e.label,
-          ),
-        )
-      ],
-      selectedItemColor: context.colorTheme.indigoDark,
-      iconSize: 40,
-    );
+
+    return SafeArea(
+        child: Container(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: context.colorTheme.onyx.withOpacity(0.9),
+        borderRadius: BorderRadius.circular(56),
+      ),
+      child: ClipRRect(
+        child: BackdropFilter(
+            filter: ui.ImageFilter.blur(
+              sigmaX: 2.5,
+              sigmaY: 2.5,
+            ),
+            child: Center(
+              child: BottomNavigationBar(
+                currentIndex: activeTab.index,
+                onTap: (value) => ref
+                    .read(bottomNavbarControllerProvider.notifier)
+                    .setTab(BottomNavbarEnum.values[value]),
+                items: [
+                  ...BottomNavbarEnum.values.map((e) {
+                    return BottomNavigationBarItem(
+                      icon: Center(
+                        child: Icon(
+                          e.icon,
+                          size: 40,
+                        ),
+                      ),
+                      label: e.label,
+                    );
+                  }),
+                ],
+                selectedItemColor: context.colorTheme.indigoLight,
+                unselectedItemColor: context.colorTheme.lightGrey,
+                iconSize: 40,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+            )),
+      ),
+    ));
   }
 }
