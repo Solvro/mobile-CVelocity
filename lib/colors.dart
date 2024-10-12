a0da23e1f79ddca4020a69a40f4afb6d212a942c
Szymon Kowaliński <szymon@kowalinski.dev>
2024-10-12 12:50:21 +0200

    feat: add shadn random theme

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+abstract class CVColors {
+  static const floralWhite = Color(0xFFFEF9EF);
+  static const richBlack = Color(0xff001524);
+  static const caribbeanGreen = Color(0xff15616D);
+  static const orangeWheel = Color(0xffFF7D00);
+  static const siennaBrown = Color(0xff78290F);
+  static const platinumLightGrey = Color(0xFFDADDD8);
+  static const burgundyRed = Color(0xFF8C172A);
+}
