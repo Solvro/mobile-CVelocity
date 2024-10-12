cb7865c19d83bb0a55090ececda70fc7b1f28a0d
tomasz-trela <trela.tomasz@protonmail.com>
2024-10-12 13:35:24 +0200

    feat: app_bar

---
+++ lib/widgets/logo_app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:cvelocity/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+class LogoAppBar extends AppBar {
+  LogoAppBar(
+    BuildContext context, {
+    super.key,
+  }) : super(
+          title: const AppBarLogo(),
+          centerTitle: false,
+          titleSpacing: 0,
+          backgroundColor: CVColors.floralWhite,
+          scrolledUnderElevation: 0,
+          automaticallyImplyLeading: false,
+        );
+}
+
+class AppBarLogo extends StatelessWidget {
+  
+  const AppBarLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 48),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: 100,
+            height: 50,
+            child: Placeholder(),
+          ),
+          ShadAvatar(
+            'https://app.requestly.io/delay/2000/avatars.githubusercontent.com/u/124599?v=4',
+            placeholder: Text('CN'),
+          ),
+        ],
+      ),
+    );
+  }
+}
