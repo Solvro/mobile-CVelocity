92bb7367a60f084bc7b29688fbba450116f8b3d8
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-10-13 01:24:22 +0200

    feat:color changed

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 abstract class CVColors {
-  static const indigoLight = Color(0xFF610F7F);
+  static const indigoLight = Color(0xFF7357DD);
   static const indigoDark = Color(0xFF480863);
   static const russianViolet = Color(0xFF2F0147);
   static const onyx = Color(0xFF1E1E1E);
